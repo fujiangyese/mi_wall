ff20a9a5763f85552a72976b036e196acb75541d
fujiangyese <he123789456@gmail.com>
2020-09-15 18:06:52 +0800

    1、cnpm i axios --save-dev 安装了axios可以发起网络请求了 2、恢复了HelloWorld.vue文件到初始状态

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,6 @@
 <template>
 	<div class="hello">
 		<h1>{{ msg }}</h1>
-		<ul v-show="isShow">
-			<li v-for="data in data_list" :key="data.id">
-				<strong>这是传递过来的一条数据</strong>
-				<div class="list">
-					<p>{{ data.name }}</p>
-					|
-					<span>{{ data.hobby }}</span>
-				</div>
-			</li>
-		</ul>
-		<button @click="clearData">清空以上数据</button>
-		<button @click="changeStatus">显示隐藏</button>
 	</div>
 </template>
 
@@ -20,43 +8,14 @@
 export default {
 	name: "HelloWorld",
 	data() {
-		return {
-			data_list: [
-				{ id: 1, name: "Vue First", hobby: "Eat food" },
-				{ id: 2, name: "Vue second", hobby: "Play football" },
-				{ id: 3, name: "Jane", hobby: "Sing" },
-			],
-			isShow: true,
-		};
+		return {};
 	},
 	props: {
 		msg: String,
 	},
-	methods: { 
-		clearData() {
-			// 将data中的数据都清空
-			this.msg = "";
-			this.data_list = [];
-		},
-		changeStatus() {
-			this.isShow = !this.isShow;
-			console.log(this.isShow);
-		},
-	},
+	methods: {},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-button {
-	color: cornflowerblue;
-	background: white;
-}
-button:hover {
-	color: white;
-	background: cornflowerblue;
-}
-.list > p {
-	display: inline;
-}
-</style>
+<style scoped></style>
