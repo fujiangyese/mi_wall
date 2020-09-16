08e4efde71c34df81747c678f6565c4026a744b6
yesefujiang <he123789456@gmail.com>
2020-09-16 16:41:49 +0800

    watch监听的使用 在watch属性中通过监听对象:函数的方式来监听 回调函数可以接收to,from两个参数

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<button @click="testWatch">watch的使用</button>
+	</div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+	name: "App",
+	components: {
+		HelloWorld,
+	},
+	data() {
+		return {
+			watcher_data: {'name':'小明'},
+		};
+	},
+	methods: {
+		testWatch() {
+			this.watcher_data.name = "老王";
+		},
+	},
+	watch: {
+		'watcher_data.name':function(to,from){
+      console.log(to,from)
+    }
+	},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
