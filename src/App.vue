1a44cd38b17e277a88fbcbf14bb6fb2981abe18a
yesefujiang <he123789456@gmail.com>
2020-09-16 17:54:58 +0800

    filters使用

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <input type="text" v-model="message">{{message|capitalize}}
   </div>
 </template>
-
+// filters 过滤器，使用范围：插值表达式，v-bind表达式(在2.1版本后才有) 
+// filters 作用是将前面的数据进行过滤返回，用法：在数据变量后加上|跟上具体的filter
+// filters 写法：在filters属性中写上具体的过滤函数，需要接收一个参数value，代表当前变量
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 
@@ -12,6 +15,19 @@ export default {
   name: "App",
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      message:''
+    }
+  },
+  filters:{
+    capitalize(value){
+      if (!value){
+        return ''
+      }
+      return value[0].toUpperCase()+value.slice(1)
+    }
   }
 };
 </script>
