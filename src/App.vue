de5e474be89b8a8b4b13a3f5509599203a04318f
yesefujiang <he123789456@gmail.com>
2020-09-17 19:31:48 +0800

    给master分支增加上vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <!-- <router-link :to="{name:'About',query:{age:18}}">跳转到About</router-link> -->
-    <router-link :to="{name:'About',query:{age:18}}">H1和H2</router-link>
-    <router-link :to="{name:'Change',query:{age:18}}">H2和H1</router-link>
-
+    <h1>{{msg}}</h1>
     <router-view></router-view>
-    <router-view name="left"></router-view>
-    <router-view name="right"></router-view>
-    <button @click="goToLastPage">回到上一个页面</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld
   },
   data(){
     return {
@@ -27,9 +16,6 @@ export default {
     }
   },
   methods: {
-    goToLastPage(){
-      this.$router.go(-1)   //跳转到上一个页面
-    }
   },
 };
 </script>
